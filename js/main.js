580e5100334da40cc1f570a9cbb8a14652e5643a
JollyUmbra <mwooddesigns@gmail.com>
2015-05-06 15:17:42 -0400

    Removed coffeescript useage and added content/updated styles and media queries.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,72 @@
-$(document).ready(function(){var t,e,i,l,n,h,o,r,s,a,f,d,c,w;for(d=$(this).scrollTop(),h=0,o=$("#content").offset().top,$(this).scroll(function(){var t;d=$(this).scrollTop(),$("#hero").css({transform:"translateY("+-d/3+"px)","-webkit-transform":"translateY("+-d/3+"px)"}),t=1,h>=d?t=1:o>=d&&(t=1-d/o),$("#hero").css({opacity:t})}),e=document.getElementById("headerCanvas"),i=e.getContext("2d"),e.style.width="100%",e.style.height="100%",e.width=window.innerWidth,e.height=window.innerHeight,t="#6590dc",i.fillStyle=t,i.fillRect(0,0,e.width,e.height),w=1,n=.1,s=0,c=2,l="hsla("+s+", 100%, 50%, 1)",f=[],a=0;a<e.width;)f.push(0),a++;return r=setInterval(function(){var t,h;for(i.fillStyle="#fff",t=0;t<e.width;){if(l="hsla("+s+", 100%, 50%, 1)",i.fillStyle=l,i.fillRect(t,0,1,f[t]),f[t]+=Math.random()*c,f[0]>e.height+200)for(h in f)f[h]=0,s+=.3;t++}w+=n},1)});
+$(document).ready(function()
+{
+  var BG, c, ctx, currentColor, dt, fadeStart, fadeUntil, headerAnim, hue, i, lines, scrollPos, speed, time;
+  scrollPos = $(this).scrollTop();
+  fadeStart = 0;
+  fadeUntil = $('#content').offset().top;
+  $(this).scroll(function()
+  {
+    var newOpacity;
+    scrollPos = $(this).scrollTop();
+    $('#hero').css({
+      'transform': 'translateY(' + -scrollPos / 3 + 'px)',
+      '-webkit-transform': 'translateY(' + -scrollPos / 3 + 'px)'
+    });
+    newOpacity = 0;
+    if (scrollPos <= fadeStart)
+    {
+      newOpacity = 1;
+    }
+    else if (scrollPos <= fadeUntil)
+    {
+      newOpacity = 1 - scrollPos / fadeUntil;
+    }
+    $('#hero').css({
+      opacity: newOpacity
+    });
+  });
+  c = document.getElementById('headerCanvas');
+  ctx = c.getContext("2d");
+  c.style.width = "100%";
+  c.style.height = "100%";
+  c.width = window.innerWidth;
+  c.height = window.innerHeight;
+  BG = '#6590dc';
+  ctx.fillStyle = BG;
+  ctx.fillRect(0, 0, c.width, c.height);
+  time = 1;
+  dt = 0.1;
+  hue = 0;
+  speed = 2;
+  currentColor = 'hsla(' + hue + ', 100%, 50%, 1)';
+  lines = [];
+  i = 0;
+  while (i < c.width)
+  {
+    lines.push(0);
+    i++;
+  }
+  return headerAnim = setInterval((function()
+  {
+    var j, num;
+    ctx.fillStyle = '#fff';
+    j = 0;
+    while (j < c.width)
+    {
+      currentColor = 'hsla(' + hue + ', 100%, 50%, 1)';
+      ctx.fillStyle = currentColor;
+      ctx.fillRect(j, 0, 1, lines[j]);
+      lines[j] += Math.random() * speed;
+      if (lines[0] > c.height + 200)
+      {
+        for (num in lines)
+        {
+          lines[num] = 0;
+          hue += 0.3;
+        }
+      }
+      j++;
+    }
+    time += dt;
+  }), 1);
+});
